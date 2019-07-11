333f07dd4729dcb076fd9fb46d6160efd24e53cc
Liv-C <52643588+Liv-C@users.noreply.github.com>
2019-07-11 14:23:51 +0800

    Add files via upload

---
+++ HW1/2019.07.11.R
@@ -0,0 +1,7 @@
+library(dplyr)
+library(downloader)
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/msleep_ggplot2.csv"
+filename <- "msleep_ggplot2.csv"
+if (!file.exists(filename)) download(url,filename)
+msleep <- read.csv("msleep_ggplot2.csv")
+head(msleep)
